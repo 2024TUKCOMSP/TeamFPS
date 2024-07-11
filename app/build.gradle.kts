4d06705efe292071b213422b4aa8c193372c0440
Baaunn <zxdr567@naver.com>
2024-07-11 22:16:00 +0900

    Add naver login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@ android {
     namespace = "com.example.myapplication"
     compileSdk = 34
 
+    viewBinding { enable = true }
+
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 28
@@ -36,6 +38,23 @@ android {
 }
 
 dependencies {
+    //네이버 로그인
+    implementation("com.navercorp.nid:oauth:5.9.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.legacy:legacy-support-core-utils:1.0.0")
+    implementation("androidx.browser:browser:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.2.1")
+    implementation("com.airbnb.android:lottie:3.1.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,4 +64,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,108 @@
+package com.example.myapplication
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityLoginBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.NaverIdLoginSDK.oauthLoginCallback
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        //네아로 객체 초기화
+        NaverIdLoginSDK.initialize(this, getString(R.string.naver_client_id),
+            getString(R.string.naver_client_secret), getString(R.string.naver_client_name))
+
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onSuccess() {
+                // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+//                binding.tvAccessToken.text = NaverIdLoginSDK.getAccessToken()
+//                binding.tvRefreshToken.text = NaverIdLoginSDK.getRefreshToken()
+//                binding.tvExpires.text = NaverIdLoginSDK.getExpiresAt().toString()
+//                binding.tvType.text = NaverIdLoginSDK.getTokenType()
+//                binding.tvState.text = NaverIdLoginSDK.getState().toString()
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(applicationContext,"errorCode:$errorCode, errorDesc:$errorDescription",Toast.LENGTH_SHORT).show()
+            }
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+
+        binding.naverLogin.setOnClickListener {
+            NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+        }
+
+        binding.naverLogout.setOnClickListener {
+            NaverIdLoginSDK.logout()
+        }
+
+        binding.naverDelete.setOnClickListener {
+            NidOAuthLogin().callDeleteTokenApi(object : OAuthLoginCallback {
+                override fun onSuccess() {
+                    //서버에서 토큰 삭제에 성공한 상태입니다.
+                }
+                override fun onFailure(httpStatus: Int, message: String) {
+                    // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                    // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                    Log.d(TAG, "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                    Log.d(TAG, "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+                }
+                override fun onError(errorCode: Int, message: String) {
+                    // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                    // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                    onFailure(errorCode, message)
+                }
+            })
+        }
+
+//        binding.kakaoLogin.setOnClickListener {
+//            // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+//            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+//                if (error != null) {
+//                    Log.e(TAG, "카카오계정으로 로그인 실패", error)
+//                } else if (token != null) {
+//                    Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+//                }
+//            }
+//
+//            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+//            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+//                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+//                    if (error != null) {
+//                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+//
+//                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+//                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+//                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+//                            return@loginWithKakaoTalk
+//                        }
+//
+//                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+//                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//                    } else if (token != null) {
+//                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+//                    }
+//                }
+//            } else {
+//                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//            }
+//        }
+    }
+}
