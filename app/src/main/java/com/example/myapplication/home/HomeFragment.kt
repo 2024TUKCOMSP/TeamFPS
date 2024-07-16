84e33aeb459644422c532919e8f3018dca73d95a
Baaunn <zxdr567@naver.com>
2024-07-16 17:26:45 +0900

    Add recylerview at homefragment

--- app/src/main/java/com/example/myapplication/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/home/HomeFragment.kt
@@ -10,49 +10,85 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.databinding.RecyclerItemBinding
 
 class HomeFragment : Fragment() {
-
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val costs = mutableListOf<String>()
+        for (i in 1..45) {
+            costs.add("Item $i")
+        }
+
+        binding.paintingList.layoutManager = LinearLayoutManager(context)
+        val adapter = ResultAdapter(costs)
+        binding.paintingList.adapter = adapter
+        binding.paintingList.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        var plusbutton = view.findViewById<Button>(R.id.buy_painting)
-        var container = requireContext()
-        plusbutton.setOnClickListener{
-            addLayout() //미완 파베 등록되거나 삭제 되면 완성본이 올라갔으면 좋겠음
-        }
+    class RecyclerHolder(val binding: RecyclerItemBinding): RecyclerView.ViewHolder(binding.root)
 
-    }
-    private fun addLayout()
-    {
-        var pluslayout = view?.findViewById<LinearLayout>(R.id.drawlayout)
-        val newLayout = LinearLayout(requireContext()).apply {
-            orientation = LinearLayout.HORIZONTAL
-        }
+    class ResultAdapter(val costs: MutableList<String>):
+        RecyclerView.Adapter<RecyclerHolder>(){
 
-        // 새 레이아웃에 위젯 추가
-        val label = TextView(requireContext()).apply {
-            text = "New Label:"
-        }
-        val editText = EditText(requireContext())
-        val button = Button(requireContext()).apply {
-            text = "New Button"
-        }
+//        private val VIEW_TYPE_ITEM = 0
+//        private val VIEW_TYPE_LOADING = 1
 
-        newLayout.addView(label)
-        newLayout.addView(editText)
-        newLayout.addView(button)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder =
+            RecyclerHolder(RecyclerItemBinding.inflate(LayoutInflater.from(parent.context),
+                parent, false))
 
-        // 기존의 drawlayout에 새 레이아웃 추가
-        pluslayout?.addView(newLayout)
+        override fun getItemCount(): Int = costs.size
+
+        override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
+            holder.binding.paintings.setImageDrawable(R.drawable.kakao_login_medium_narrow.toDrawable())
+            holder.binding.textview.text = costs[position]
+        }
     }
 
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        var plusbutton = view.findViewById<Button>(R.id.buy_painting)
+//        var container = requireContext()
+//        plusbutton.setOnClickListener{
+//            addLayout() //미완 파베 등록되거나 삭제 되면 완성본이 올라갔으면 좋겠음
+//        }
+//
+//    }
 
+//    private fun addLayout()
+//    {
+//        var pluslayout = view?.findViewById<LinearLayout>(R.id.drawlayout)
+//        val newLayout = LinearLayout(requireContext()).apply {
+//            orientation = LinearLayout.HORIZONTAL
+//        }
+//
+//        // 새 레이아웃에 위젯 추가
+//        val label = TextView(requireContext()).apply {
+//            text = "New Label:"
+//        }
+//        val editText = EditText(requireContext())
+//        val button = Button(requireContext()).apply {
+//            text = "New Button"
+//        }
+//
+//        newLayout.addView(label)
+//        newLayout.addView(editText)
+//        newLayout.addView(button)
+//
+//        // 기존의 drawlayout에 새 레이아웃 추가
+//        pluslayout?.addView(newLayout)
+//    }
 }
