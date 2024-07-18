be4c7d149d164231fcdc45b940a0c5afebb71d92
YangDongGyun <ydg0724@gmail.com>
2024-07-18 17:52:53 +0900

    커스텀다이얼로그 설정

--- app/src/main/java/com/example/myapplication/data/Users.kt
+++ app/src/main/java/com/example/myapplication/data/Users.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.data
 data class Users(
     var uid: String? = "",
     var name: String? = "",
-    var email: String? = "",
+    var nickname: String? = "",
     var profilePictureUrl: String? = "",
-    var authprovider: String? =""
+    //var authprovider: String? =""
 )
