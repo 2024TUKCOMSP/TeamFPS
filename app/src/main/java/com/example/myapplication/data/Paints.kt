c097b72050cb98ebec874852659642e772025470
Baaunn <zxdr567@naver.com>
2024-07-18 19:35:43 +0900

    Update recylerview and add func getting data form firebase

---
+++ app/src/main/java/com/example/myapplication/data/Paints.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.data
+
+data class Paints(
+    var pid: String? = "",
+    var uid: String? = "",
+    var cost: String? = ""
+)
--- app/src/main/java/com/example/myapplication/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/home/HomeFragment.kt
@@ -10,51 +10,80 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.data.Paints
+import com.example.myapplication.data.Users
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.example.myapplication.databinding.RecyclerItemBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
+    lateinit var firebaseRef: DatabaseReference
+    lateinit var paintList: MutableList<Paints>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val costs = mutableListOf<String>()
-        for (i in 1..45) {
-            costs.add("Item $i")
-        }
+        firebaseRef = FirebaseDatabase.getInstance().getReference("paints")
+        paintList = mutableListOf<Paints>()
 
+        fetchData()
         binding.paintingList.layoutManager = LinearLayoutManager(context)
-        val adapter = ResultAdapter(costs)
-        binding.paintingList.adapter = adapter
         binding.paintingList.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
 
         return binding.root
     }
 
+    private fun fetchData() {
+        firebaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                paintList.clear()
+                if (snapshot.exists()) {
+                    for (paintSnap in snapshot.children) {
+                        val paints = paintSnap.getValue(Paints::class.java)
+                        paintList.add(paints!!)
+                        val adapter = ResultAdapter(paintList)
+                        binding.paintingList.adapter = adapter
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "error: $error", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
     class RecyclerHolder(val binding: RecyclerItemBinding): RecyclerView.ViewHolder(binding.root)
 
-    class ResultAdapter(val costs: MutableList<String>):
+    class ResultAdapter(val paintList: MutableList<Paints>):
         RecyclerView.Adapter<RecyclerHolder>(){
-
-//        private val VIEW_TYPE_ITEM = 0
-//        private val VIEW_TYPE_LOADING = 1
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder =
             RecyclerHolder(RecyclerItemBinding.inflate(LayoutInflater.from(parent.context),
                 parent, false))
 
-        override fun getItemCount(): Int = costs.size
+        override fun getItemCount(): Int = paintList.size
 
         override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
+            val currentPaint = paintList[position]
             holder.binding.paintings.setImageDrawable(R.drawable.kakao_login_medium_narrow.toDrawable())
-            holder.binding.textview.text = costs[position]
+            holder.apply {
+                binding.apply {
+                    textview.text = currentPaint.cost
+                }
+            }
         }
     }
 
